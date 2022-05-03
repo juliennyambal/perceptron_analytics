8aee99b8156f278ffbca9ee25656c7673b14f75b
Julien Nyambal <Julien.nyambal@standardbank.co.za>
2022-05-04 01:45:42 +0200

    adding derivative sigmoid

--- perceptron.py
+++ perceptron.py
@@ -10,13 +10,18 @@ import numpy as np
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+def predict(a):
+    h = a[0]*w_1 + a[1]*w_2 + w_b
+    return sigmoid(h)
+
 w_1 = 0.23
 
 w_2 = 0.12
 
 w_b = 0.01
-
-# weights = np.array([0.01, 0.12, 0.23])
 
 lr = 0.25
 
@@ -29,15 +34,17 @@ labels = np.array([0, 1, 1, 1])
 
 error_lst = [10]
 error = 0.1
-while error > 0.00001:
+while error > 0.000000001:
     for x, y, in zip(input_set, labels):
     
         h = x[1]*w_1 + x[2]*w_2 + x[0]*w_b
         y_hat = sigmoid(h)
         
-        delta_w_b = x[0] * y_hat * (1 - y_hat) * (y_hat - y)
-        delta_w_1 = x[1] * y_hat * (1 - y_hat) * (y_hat - y)
-        delta_w_2 = x[2] * y_hat * (1 - y_hat) * (y_hat - y)
+        # Gradient bias
+        delta_w_b = x[0] * sigmoid_prime(h) * (y_hat - y)
+        # Gradients weights
+        delta_w_1 = x[1] * sigmoid_prime(h) * (y_hat - y)
+        delta_w_2 = x[2] * sigmoid_prime(h) * (y_hat - y)
     
         w_b = w_b - lr * delta_w_b
         w_1 = w_1 - lr * delta_w_1
@@ -50,9 +57,7 @@ while error > 0.00001:
         
     print(error)
 
-def predict(a):
-    h = a[0]*w_1 + a[1]*w_2 + w_b
-    return sigmoid(h)
+
 
 print("")
 print(predict([0,0]))
